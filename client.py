7dad101d32a7e4cb1baa47eb533f84c03dbd9b4b
SmellyCat <smellycat2014@foxmail.com>
2015-12-25 11:37:06 +0800

    更新 客户端测试

--- client.py
+++ client.py
@@ -1,14 +1,12 @@
 from socketIO_client import SocketIO, BaseNamespace
 
-def on_response(*args):
-    print('on_response', args)
+class TestNamespace(BaseNamespace):
+    def on_response(*args):
+        print('on_response', args)
 
-print 'test'
-socket = SocketIO('localhost', 5000)
-chat = socket.define(BaseNamespace, '/test')
-print 'test1'
-chat.emit('my event')
-chat.on('my response', on_response)
+
+socket = SocketIO('10.0.0.7', 5000)
+socket.emit('my event', {'data': 'I\'m connected.'})
 
 ##from socketIO_client import SocketIO, BaseNamespace
 ##
